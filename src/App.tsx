55c3ce004be367c1fa86af955806ebcf9d527891
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-20 21:21:34 +0200

    added function to be able to input text fixed too long words are wrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function App() {
+
+  /** @const {React.RefObject<HTMLTextAreaElement>} refInput Reference of the input field */
+  const refInput = useRef<HTMLTextAreaElement>(null);
+
+  /** @const {string} inputText That typed text */
+  const [inputText, setInputText] = useState<string>('');
+
+  /** Prevents textarea to loose focus */
+  useEffect(() => {
+    if (refInput && refInput.current) {
+      refInput.current.focus();
+
+      refInput.current.onblur = () => {
+        refInput.current?.focus();
+      }
+    }
+  }, []);
+
+  /**
+   * Saves the typed text
+   * @param e 
+   */
+  const handleType = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setInputText(e.target.value);
+  }
+
+  // -----------------------------------------
+
   return (
     <main>
       <div className="background"></div>
 
       <div>
-        <span>Working</span>
+        <span>{inputText}</span>
       </div>
       <div className="text-shine">
-        <span>Working</span>
+        <span>{inputText}</span>
       </div>
+
+      <textarea ref={refInput} onChange={handleType}></textarea>
     </main>
   );
 }
